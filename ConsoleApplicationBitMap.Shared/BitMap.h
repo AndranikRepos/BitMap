52b76bd400f123e1434de3030d6df87b22fe0dba
AndranikRepos <andranik.vartanyan.prog@gmail.com>
2023-02-12 12:42:58 +0400

    changes

--- ConsoleApplicationBitMap.Shared/BitMap.h
+++ ConsoleApplicationBitMap.Shared/BitMap.h
@@ -11,6 +11,13 @@
 #endif
 
 #define __TEM_T template < typename T
+#define __TEM_T_ALLOC __TEM_T , template < typename > typename Alloc
+#define TEM_DEF __TEM_T_ALLOC = std::allocator >
+#define TEM __TEM_T_ALLOC >
+#define TEM_U template < typename U >
+#define __TEM_CONT template < typename Cont , typename
+#define TEM_CONT __TEM_CONT >
+#define TEM_CONT_DEF __TEM_CONT = std::enable_if_t <	\
 
 namespace Containers
 {
