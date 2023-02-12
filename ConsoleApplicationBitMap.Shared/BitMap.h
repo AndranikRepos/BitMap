032aee3187258345570aa6acf0eaf4252dd6db3e
AndranikRepos <andranik.vartanyan.prog@gmail.com>
2023-02-12 13:05:34 +0400

    changes

--- ConsoleApplicationBitMap.Shared/BitMap.h
+++ ConsoleApplicationBitMap.Shared/BitMap.h
@@ -18,10 +18,24 @@
 #define __TEM_CONT template < typename Cont , typename
 #define TEM_CONT __TEM_CONT >
 #define TEM_CONT_DEF __TEM_CONT = std::enable_if_t <	\
+	!std::is_same_v < std::decay_t < Cont > , common_iterator < IsConst > >	\
+	&& !std::is_base_of_v < common_iterator < IsConst > , std::decay_t < Cont > >	\
+	>	\
+>
+#define SIZE_TYPE std::size_t
+#define INT_TYPE int32_t
 
 namespace Containers
 {
 	TEM_DEF class DLLEXPORT BitMap
 	{
+	public:
+		using value_type = T;
+		using reference = value_type&;
+		using const_reference = const reference;
+		using pointer = value_type*;
+		using const_pointer = const pointer;
+		using size_type = SIZE_TYPE;
+		using int_type = INT_TYPE;
 	};
 }
